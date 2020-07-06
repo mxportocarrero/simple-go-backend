2b7adc1838d37d07a3f5f73b70d2eaf133551607
Max W. Portocarrero <mxportocarrero@gmail.com>
2020-07-06 06:17:53 -0500

    added godotenv package and small configuartion on environment packages

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"simple-go-backend/internal/api"
 	"simple-go-backend/internal/config"
 
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		logrus.WithError(err).Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
 	logrus.SetLevel(logrus.DebugLevel)
-	logrus.WithField("version", config.Version).Debug("Starting server...")
+	logrus.WithField("version", config.Version).Debug(fmt.Sprintf("Starting server at localhost:%v", port))
 
 	router, err := api.NewRouter()
 	if err != nil {
 		logrus.WithError(err).Fatal("Error building router")
 	}
 
-	address := "127.0.0.1:8000"
+	address := fmt.Sprintf("127.0.0.1:%v", port)
 	server := http.Server{
 		Handler: router,
 		Addr:    address,
 	}
+
+	logrus.Debug("listening..")
 
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		logrus.WithError(err).Error("Server failed!")
